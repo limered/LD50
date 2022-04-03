481aabd6531d13da48906114a2224915de956493
Emil Bohleber <emil.bohleber@brickmakers.de>
2022-04-03 03:38:50 +0200

    add achievement list

---
+++ Why is it always Projekt/Assets/Systems/Achievements/AchievementRenderComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SystemBase.Core;
+using UnityEngine;
+
+namespace Systems.Achievements
+{
+    public class AchievementRenderComponent : GameComponent
+    {
+        [SerializeField] public List<Achievement> achievements = new List<Achievement>();
+    }
+}
---
+++ Why is it always Projekt/Assets/Systems/Achievements/AchievementSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SystemBase.Core;
+using UniRx;
+
+namespace Systems.Achievements
+{
+    [GameSystem]
+    public class AchievementSystem : GameSystem<AchievementRenderComponent>
+    {
+        public override void Register(AchievementRenderComponent component)
+        {
+            MessageBroker.Default.Receive<AchievementMessage>().Subscribe(msg =>
+            {
+                var achievement =
+                    component.achievements.Find(achievement1 => achievement1.name == msg.Achievement.name);
+                if(achievement != null)
+                {
+                    achievement.achieved = true;
+                }
+            }).AddTo(component);
+        }
+    }
+
+    public class AchievementMessage
+    {
+        public Achievement Achievement { get; set; }
+    }
+    
+    [Serializable]
+    public class Achievement
+    {
+        public string name;
+        public string text;
+        public bool achieved;
+    }
+}
